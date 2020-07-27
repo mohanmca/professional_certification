735b6927d6cd74a4544639aa41a3bbfcb1907b2c
Mohan Narayanaswamy <mohanmca@gmail.com>
2020-07-28 04:38:42 +0800

    AWS

--- aws/anki_creator.bat
+++ aws/anki_creator.bat
@@ -3,3 +3,4 @@ rm aws_merged_anki.md
 cat *anki*.md >> aws_merged_ikna.md
 mv aws_merged_ikna.md aws_merged_anki.md
 mdanki aws_merged_anki.md aws_merged_anki.apkg --deck "AWS SAA Certification::MdAnki::SAA-CA02"
+pandoc aws_merged_anki.md --pdf-engine "D:\Apps\pandoc-2.10.1\mitex\miktex\bin\x64\pdflatex.exe" -o aws.pdf
