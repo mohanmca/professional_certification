3ec49828fba52f6c5163402b8fbf1dfdd6d15eba
Mohan Narayanaswamy <mohanmca@gmail.com>
2021-06-19 21:28:19 +0800

    SAP

--- algorithms/Course/Princeton_Algorithm/wordnet/SAP.java
+++ algorithms/Course/Princeton_Algorithm/wordnet/SAP.java
@@ -5,43 +5,71 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
-    Digraph graph;
+
+    private DepthFirstDiGraph[] graphPaths;
+    private Digraph graph;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph g) {
-        graph = g;
+        this.graph = g;
+        graphPaths = new DepthFirstDiGraph[g.V()];
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-
+        int parent = ancestor(v, w);
+        if (parent != -1)
+            return graphPaths[v].pathTo(parent).size() + graphPaths[w].pathTo(parent).size() - 2;
+        return parent;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        DepthFirstDiGraph digraphV = new DepthFirstDiGraph(graph, v);
-        DepthFirstDiGraph dfsPathsOfB = new DepthFirstDiGraph(graph, w);
-        for (Integer x : digraphV.pathToAncestor()) {
-            if (dfsPathsOfB.hasPathTo(x)) {
-                return x;
+        if (v >= graph.V() || w >= graph.V()) return -1;
+        if (graphPaths[v] == null)
+            graphPaths[v] = new DepthFirstDiGraph(graph, v);
+        if (graphPaths[w] == null)
+            graphPaths[w] = new DepthFirstDiGraph(graph, w);
+        if (graphPaths[v].getAncestor() != -1 && graphPaths[w].getAncestor() != -1) {
+            for (Integer x : graphPaths[v].pathToAncestor()) {
+                if (graphPaths[w].hasPathTo(x)) {
+                    return x;
+                }
             }
         }
         return -1;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w)
+    public int length(Iterable<Integer> vs, Iterable<Integer> ws) {
+        int min = 99999;
+        for (int v : vs) {
+            for (int w : ws) {
+                min = Math.min(length(v, w), min);
+            }
+        }
+        return min == 99999 ? -1 : min;
+    }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
-
-    // do unit testing of this class
-
-    public static void main(String[] args) {
-
+    public int ancestor(Iterable<Integer> vs, Iterable<Integer> ws) {
+        int min = 99999;
+        int parent = -1;
+        for (int v : vs) {
+            for (int w : ws) {
+                int len = length(v, w);
+                if (len != -1 && len < min) {
+                    parent = ancestor(v, w);
+                }
+            }
+        }
+        return parent;
     }
 
 
@@ -49,7 +77,7 @@ public class SAP {
         private Digraph graph;
         private boolean[] marked;
         private Integer[] edgeTo;
-        private int ancestor = -99;
+        private int ancestor = -1;
         private int s;
 
         public DepthFirstDiGraph(Digraph g, int s) {
@@ -68,7 +96,7 @@ public class SAP {
                     dfs(w);
                 }
             }
-            if (ancestor == -99) ancestor = v;
+            if (ancestor == -1) ancestor = v;
         }
 
         private Iterable<Integer> pathToAncestor() {
@@ -79,7 +107,7 @@ public class SAP {
             return marked[v];
         }
 
-        private Iterable<Integer> pathTo(int v) {
+        private Stack<Integer> pathTo(int v) {
             Stack<Integer> trace = new Stack<>();
             for (int x = v; x != s; x = edgeTo[x]) {
                 trace.push(x);
@@ -90,6 +118,19 @@ public class SAP {
 
         private int getAncestor() {
             return ancestor;
+        }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
         }
     }
 
--- algorithms/Course/Princeton_Algorithm/wordnet/WordNet.java
+++ algorithms/Course/Princeton_Algorithm/wordnet/WordNet.java
@@ -8,22 +8,26 @@ import edu.princeton.cs.algs4.DepthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stack;
-import edu.princeton.cs.algs4.TransitiveClosure;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class WordNet {
     private Map<String, Integer> synsetMap;
     private Map<Integer, String> synsetReverseMap;
     private Digraph graph;
-    TransitiveClosure tc;
+    private SAP shortestAncestorPathFinder;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        if (synsets == null || hypernyms == null)
+            throw new IllegalArgumentException("Arguments should not be null!");
         populateSynsetMap(synsets);
+
         graph = new Digraph(synsetReverseMap.size());
+
         In hypIn = new In(hypernyms);
         while (hypIn.hasNextLine()) {
             int[] columns = Arrays.stream(hypIn.readLine().split(",")).map(Integer::parseInt)
@@ -32,20 +36,26 @@ public class WordNet {
                 graph.addEdge(columns[0], columns[1]);
             }
         }
-        tc = new TransitiveClosure(graph);
+
+        DepthFirstDirectedCycle cycleFinder = new DepthFirstDirectedCycle(graph);
+        if (cycleFinder.hasCycle()) {
+            throw new IllegalArgumentException("Graph is not DAG, It has cycle!");
+        }
+        shortestAncestorPathFinder = new SAP(graph);
     }
 
-    private Map<String, Integer> populateSynsetMap(String synsets) {
+    private void populateSynsetMap(String synsets) {
         In syntax = new In(synsets);
         synsetMap = new HashMap<>();
         synsetReverseMap = new HashMap<>();
         int count = 0;
         while (syntax.hasNextLine()) {
             String[] columns = syntax.readLine().split(",");
-            synsetMap.put(columns[1], Integer.parseInt(columns[0]));
-            synsetReverseMap.put(count++, columns[1]);
+            for (String noun : columns[1].split(" ")) {
+                synsetMap.put(noun, Integer.parseInt(columns[0]));
+                synsetReverseMap.put(count++, noun);
+            }
         }
-        return synsetMap;
     }
 
     // returns all WordNet nouns
@@ -55,13 +65,21 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
+        if (word == null)
+            throw new IllegalArgumentException("word should not be null!");
         return synsetMap.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        DepthFirstDirectedPaths paths = new DepthFirstDirectedPaths(graph, synsetMap.get(nounA));
+        if (nounA == null || nounB == null)
+            throw new IllegalArgumentException("nouns are not allowed to be null!");
+        if (!isNoun(nounA) || !isNoun(nounB)) return Integer.MAX_VALUE;
+        if (nounA.equals(nounB)) return 0;
+        return shortestAncestorPathFinder.length(synsetMap.get(nounA), synsetMap.get(nounB));
+    }
 
+    private int lengthOfPath(String nounB, DepthFirstDirectedPaths paths) {
         int c = 0;
         for (Integer i : paths.pathTo(synsetMap.get(nounB)))
             c++;
@@ -71,13 +89,15 @@ public class WordNet {
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        DepthFirstDiGraph dfsPathA = new DepthFirstDiGraph(graph, synsetMap.get(nounA));
-        DepthFirstDiGraph dfsPathB = new DepthFirstDiGraph(graph, synsetMap.get(nounB));
-        for (Integer w : dfsPathA.pathToAncestor()) {
-            if (dfsPathB.hasPathTo(w)) {
-                return synsetReverseMap.get(w);
-            }
+        if (nounA == null || nounB == null)
+            throw new IllegalArgumentException("nouns are not allowed to be null!");
+
+        int parent = shortestAncestorPathFinder
+                .ancestor(synsetMap.get(nounA), synsetMap.get(nounB));
+        if (parent != -1) {
+            return synsetReverseMap.get(parent);
         }
+
         throw new RuntimeException();
     }
 
@@ -91,52 +111,48 @@ public class WordNet {
         System.out.println(net.sap("corn_gluten", "elastin"));
     }
 
-    private static class DepthFirstDiGraph {
-        private Digraph graph;
+    private static class DepthFirstDirectedCycle {
         private boolean[] marked;
         private Integer[] edgeTo;
-        private int ancestor = -99;
-        private int s;
+        private boolean[] onStack;
+        private Stack<Integer> cycle;
 
-        public DepthFirstDiGraph(Digraph g, int s) {
-            marked = new boolean[g.V()];
-            edgeTo = new Integer[g.V()];
-            graph = g;
-            this.s = s;
-            dfs(s);
-        }
+        public DepthFirstDirectedCycle(Digraph graph) {
+            marked = new boolean[graph.V()];
+            onStack = new boolean[graph.V()];
+            edgeTo = new Integer[graph.V()];
 
-        private void dfs(int v) {
-            marked[v] = true;
-            for (int w : graph.adj(v)) {
-                if (!marked[w]) {
-                    edgeTo[w] = v;
-                    dfs(w);
+            for (int v = 0; v < graph.V() && cycle == null; v++) {
+                if (!marked[v]) {
+                    dfs(graph, v);
                 }
             }
-            if (ancestor == -99) ancestor = v;
         }
 
-        private Iterable<Integer> pathToAncestor() {
-            return pathTo(ancestor);
+        public boolean hasCycle() {
+            return cycle != null;
         }
 
-        private boolean hasPathTo(int v) {
-            return marked[v];
-        }
-
-        private Iterable<Integer> pathTo(int v) {
-            Stack<Integer> trace = new Stack<>();
-            for (int x = v; x != s; x = edgeTo[x]) {
-                trace.push(x);
+        private void dfs(Digraph graph, int v) {
+            onStack[v] = true;
+            marked[v] = true;
+            for (int w : graph.adj(v)) {
+                if (cycle != null) return;
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    dfs(graph, w);
+                }
+                else if (onStack[w]) {
+                    cycle = new Stack<>();
+                    for (int x = v; x != w; x = edgeTo[x]) {
+                        cycle.push(x);
+                    }
+                    cycle.push(w);
+                    cycle.push(v);
+                    return;
+                }
             }
-            trace.push(s);
-            return trace;
-        }
-
-        private int getAncestor() {
-            return ancestor;
+            onStack[v] = false;
         }
     }
-
 }
