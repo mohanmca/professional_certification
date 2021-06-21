0c21c75445da33a6f67113f8444b6170731457d6
Mohan Narayanaswamy <mohanmca@gmail.com>
2021-06-22 07:40:48 +0800

    CourseFiles

--- algorithms/Course/Princeton_Algorithm/wordnet/SAP.java
+++ algorithms/Course/Princeton_Algorithm/wordnet/SAP.java
@@ -4,13 +4,20 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+
 public class SAP {
+
 
     private final BreathFirstDigraph[] graphPaths;
     private final Digraph graph;
@@ -25,33 +32,27 @@ public class SAP {
     public int length(int v, int w) {
         int parent = ancestor(v, w);
         if (parent != -1) {
-            return graphPaths[v].pathTo(parent).size() + graphPaths[w].pathTo(parent).size() - 2;
+            return graphPaths[v].distTo[parent] + graphPaths[w].distTo[parent];
         }
         return parent;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        if (v >= graph.V() || w >= graph.V()) return -1;
         if (graphPaths[v] == null)
             graphPaths[v] = new BreathFirstDigraph(graph, v);
         if (graphPaths[w] == null)
             graphPaths[w] = new BreathFirstDigraph(graph, w);
-        if (graphPaths[v].hasPathTo(w) && graphPaths[w].hasPathTo(v)) {
-            Stack<Integer> pathvw = graphPaths[v].pathTo(w);
-            Stack<Integer> pathwv = graphPaths[w].pathTo(v);
-            if (pathvw.size() > pathwv.size()) return v;
+        if (graphPaths[v].hasPathTo(w)
+                && graphPaths[v].distTo[w] <= graphPaths[v].distTo[graphPaths[v].getAncestor()])
             return w;
-        }
-        if (graphPaths[v].hasPathTo(w)) return w;
-        if (graphPaths[w].hasPathTo(v)) return v;
-        if (graphPaths[v].getAncestor() != -1 && graphPaths[w].getAncestor() != -1) {
-            for (Integer x : graphPaths[v].pathToAncestor()) {
-                if (graphPaths[w].hasPathTo(x)) {
-                    return x;
-                }
-            }
-        }
+        if (graphPaths[w].hasPathTo(v)
+                && graphPaths[w].distTo[v] <= graphPaths[w].distTo[graphPaths[w].getAncestor()])
+            return v;
+        if (graphPaths[v].getAncestor() != -1 && graphPaths[v].getAncestor() == graphPaths[w]
+                .getAncestor() && graphPaths[v]
+                .hasPathTo(graphPaths[w].getAncestor()))
+            return graphPaths[v].getAncestor();
         return -1;
     }
 
@@ -81,63 +82,75 @@ public class SAP {
         return parent;
     }
 
-
     private static class BreathFirstDigraph {
-        private Digraph graph;
         private boolean[] marked;
         private Integer[] edgeTo;
-        private int ancestor = -1;
-        private int s;
+        private Integer[] distTo;
+        private Integer ancestor = -1;
 
-        public BreathFirstDigraph(Digraph g, int s) {
+
+        public BreathFirstDigraph(Digraph g, Integer s) {
+            this(g, Arrays.asList(s));
+        }
+
+        public BreathFirstDigraph(Digraph g, Iterable<Integer> sources) {
             marked = new boolean[g.V()];
             edgeTo = new Integer[g.V()];
-            graph = g;
-            this.s = s;
-            bfs(s);
+            distTo = new Integer[g.V()];
+            for (Integer v : sources)
+                bfs(g, v);
         }
 
-        private void bfs(int s) {
-            java.util.ArrayDeque<Integer> deque = new java.util.ArrayDeque<Integer>();
-            marked[s] = true;
-            deque.add(s);
-            while (!deque.isEmpty()) {
-                int v = deque.poll();
-                if (graph.outdegree(v) == 0 && ancestor == -1) {
-                    ancestor = v;
-                }
-                else {
-                    for (int w : graph.adj(v)) {
-                        if (!marked[w]) {
-                            marked[w] = true;
-                            edgeTo[w] = v;
-                            deque.add(w);
-                        }
+        private void bfs(Digraph graph, Integer source) {
+            Iterable<Integer> s = Collections.singletonList(source);
+            bfs(graph, s);
+        }
+
+        // breadth-first search from multiple sources
+        private void bfs(Digraph G, Iterable<Integer> sources) {
+            Queue<Integer> q = new Queue<Integer>();
+            for (int s : sources) {
+                marked[s] = true;
+                distTo[s] = 0;
+                q.enqueue(s);
+            }
+            while (!q.isEmpty()) {
+                int v = q.dequeue();
+                for (int w : G.adj(v)) {
+                    if (!marked[w]) {
+                        edgeTo[w] = v;
+                        distTo[w] = distTo[v] + 1;
+                        marked[w] = true;
+                        q.enqueue(w);
+                    }
+                    else if (marked[w] && distTo[w] != 0 && ancestor == -1) {
+                        ancestor = w;
                     }
                 }
+                if (((Bag<Integer>) G.adj(v)).isEmpty() && ancestor == -1) {
+                    ancestor = v;
+                }
             }
-        }
-
-        private Iterable<Integer> pathToAncestor() {
-            return pathTo(ancestor);
         }
 
         private boolean hasPathTo(int v) {
             return marked[v];
         }
 
-        private Stack<Integer> pathTo(int v) {
-            Stack<Integer> trace = new Stack<>();
-            for (int x = v; x != s; x = edgeTo[x]) {
+        private Deque<Integer> pathTo(int v) {
+            ArrayDeque<Integer> trace = new ArrayDeque<Integer>();
+            int x;
+            for (x = v; distTo[x] != 0; x = edgeTo[x]) {
                 trace.push(x);
             }
-            trace.push(s);
+            trace.push(x);
             return trace;
         }
 
-        private int getAncestor() {
+        public int getAncestor() {
             return ancestor;
         }
+
     }
 
     public static void main(String[] args) {
@@ -149,6 +162,8 @@ public class SAP {
             int w = StdIn.readInt();
             int length = sap.length(v, w);
             int ancestor = sap.ancestor(v, w);
+            StdOut.printf("vertex-v = %d, ancestor = %d\n", v, sap.graphPaths[v].getAncestor());
+            StdOut.printf("vertex-w = %d, ancestor = %d\n", w, sap.graphPaths[w].getAncestor());
             StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
         }
     }
