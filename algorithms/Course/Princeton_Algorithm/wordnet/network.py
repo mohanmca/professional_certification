de633fa7d1b6374da766ac07b3b7989919a03cc4
Mohan Narayanaswamy <mohanmca@gmail.com>
2021-06-22 19:52:30 +0800

    network

---
+++ algorithms/Course/Princeton_Algorithm/wordnet/network.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def main():
+    file_name = "digraph1.txt"
+    f = open(file_name, 'r')
+    content = f.read()
+    graph = content.split("\n")
+    graph.pop()
+    g = nx.DiGraph()
+    g.add_nodes_from([i for i in graph[0]])
+    for edge in graph[2:]:
+        print(edge)
+        v, w = edge.strip().split()
+        g.add_edge(v, w)
+    nx.draw(g,with_labels=True)
+    plt.draw()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
